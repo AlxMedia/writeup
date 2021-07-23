5606156759f01beb1da7f8d55b4b16bd3266224a
Alexander Agnarson <alexander.agnarson@gmail.com>
2021-07-23 18:47:43 +0200

    Disable support for widgets block editor, regen lang

--- functions.php
+++ functions.php
@@ -72,6 +72,9 @@ if ( ! function_exists( 'writeup_setup' ) ) {
 		// Declare WooCommerce support
 		add_theme_support( 'woocommerce' );
 		
+		// Disable support for widgets block editor
+		remove_theme_support( 'widgets-block-editor' );
+		
 		// Thumbnail sizes
 		add_image_size( 'writeup-small', 160, 160, true );
 		add_image_size( 'writeup-medium', 520, 292, true );
