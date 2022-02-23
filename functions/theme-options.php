e82f5e8a395cf773b1bfc851ca94c744d9a307ce
AlxMedia <contact@alx.media>
2022-02-23 12:11:16 +0100

    Update FontAwesome customizer link to v5 brands

--- functions/theme-options.php
+++ functions/theme-options.php
@@ -670,7 +670,7 @@ Kirki::add_field( 'writeup_theme', array(
 	'label'			=> esc_html__( 'Create Social Links', 'writeup' ),
 	'description'	=> esc_html__( 'Create and organize your social links', 'writeup' ),
 	'section'		=> 'social',
-	'tooltip'		=> esc_html__( 'Font Awesome names:', 'writeup' ) . ' <a href="https://fontawesome.com/icons?d=gallery&s=brands&m=free" target="_blank"><strong>' . esc_html__( 'View All', 'writeup' ) . ' </strong></a>',
+	'tooltip'		=> esc_html__( 'Font Awesome names:', 'writeup' ) . ' <a href="https://fontawesome.com/v5/search?s=brands" target="_blank"><strong>' . esc_html__( 'View All', 'writeup' ) . ' </strong></a>',
 	'row_label'		=> array(
 		'type'	=> 'text',
 		'value'	=> esc_html__('social link', 'writeup' ),
@@ -687,7 +687,7 @@ Kirki::add_field( 'writeup_theme', array(
 		'social-icon'	=> array(
 			'type'			=> 'text',
 			'label'			=> esc_html__( 'Icon Name', 'writeup' ),
-			'description'	=> esc_html__( 'Font Awesome icons. Ex: fa-facebook ', 'writeup' ) . ' <a href="https://fontawesome.com/icons?d=gallery&s=brands&m=free" target="_blank"><strong>' . esc_html__( 'View All', 'writeup' ) . ' </strong></a>',
+			'description'	=> esc_html__( 'Font Awesome icons. Ex: fa-facebook ', 'writeup' ) . ' <a href="https://fontawesome.com/v5/search?s=brands" target="_blank"><strong>' . esc_html__( 'View All', 'writeup' ) . ' </strong></a>',
 			'default'		=> 'fa-',
 		),
 		'social-link'	=> array(
